7d41325abce5ae514e12d08cb5bacc8a05f80b69
Isaac Vallee <isaacvallee@gmail.com>
2017-09-19 20:14:51 +0000

    refactored and linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,14 @@
 // Counts tweet characters and updates available characters.
-
 $(document).ready(function() {
-
-  $(".tweetArea").on("keyup", function(event) {
-    const charLength = $(this).val().length;
+  $(".tweetArea").on("input", function(event) {
+    const counter = $(event.target).parent().find(".counter");
+    const charLength = $(event.target).val().length;
     const maxCount = 140;
-    $(this).parent().find(".counter").text(maxCount - charLength);
+    counter.text(maxCount - charLength);
     if (charLength > maxCount) {
-      $(this).parent().find(".counter").addClass("counterOver");
+      counter.addClass("counterOver");
     } else {
-      $(this).parent().find(".counter").removeClass("counterOver")
+      counter.removeClass("counterOver");
     }
-
   });
 });
-
-  // function countChars(chars) {
-  //   let output = 0
-  //   if (chars >= 140) {
-  //     output = 140 - chars;
-  //   } else {
-  //     output = 140 - chars;
-  //   }
-  //   return output;
-  // };
