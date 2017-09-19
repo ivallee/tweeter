a30825ea14aa8aaefe6a3137044b9f1a2f61f793
Isaac Vallee <isaacvallee@gmail.com>
2017-09-19 18:19:10 +0000

    start char count function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,24 @@
-$ document.ready() {
-  console.log("I'm working!");
+$(document).ready(() => {
+
+  $(".tweetArea").on("keyup", () => {
+    const charLength = $('.tweetArea')[0].value.length;
+    const maxCount = parseInt($(".counter").text());
+    console.log(charLength, maxCount - charLength);
+    console.log($(".counter").text());
+    console.log(this);
+
+  });
 });
+
+  function countChars(chars) {
+    let output = 0
+    if (chars >= 140) {
+      output = 140 - chars;
+    } else {
+      output = 140 - chars;
+    }
+    return output;
+  };
+
+
+console.log(countChars(150));
