467284cd2550cf24d7c7fc0326d6d11f78caad04
Isaac Vallee <isaacvallee@gmail.com>
2017-09-19 19:06:33 +0000

    functioning count characters with consolelog

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,21 @@
-$(document).ready(() => {
+$(document).ready(function() {
 
-  $(".tweetArea").on("keyup", () => {
-    const charLength = $('.tweetArea')[0].value.length;
+  $(".tweetArea").on("keyup", function(event) {
+    const charLength = $(this).val().length;
     const maxCount = parseInt($(".counter").text());
     console.log(charLength, maxCount - charLength);
-    console.log($(".counter").text());
-    console.log(this);
+    // console.log($(this).find(".counter"));
+
 
   });
 });
 
-  function countChars(chars) {
-    let output = 0
-    if (chars >= 140) {
-      output = 140 - chars;
-    } else {
-      output = 140 - chars;
-    }
-    return output;
-  };
-
-
-console.log(countChars(150));
+  // function countChars(chars) {
+  //   let output = 0
+  //   if (chars >= 140) {
+  //     output = 140 - chars;
+  //   } else {
+  //     output = 140 - chars;
+  //   }
+  //   return output;
+  // };
