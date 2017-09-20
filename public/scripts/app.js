56c02b44cbd0f5911aa3c5edfeea1ff3b9266092
Isaac Vallee <isaacvallee@gmail.com>
2017-09-20 20:36:59 +0000

    post route for tweets using AJAX and function for loading tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,18 +14,13 @@ $(function() {
 
     const $tweet = $("<article>").addClass("tweet");
 
-    // Constructs header and children
+    // Constructs header
     const header = $("<header>").addClass("tweet-header");
-    const avatarLink = createLink();
+    const avatar = createLink().append($("<img>").addClass("avatar").attr("src", data.user.avatars.small));
     const tweeterUsername = $("<h4>").addClass("tweeter-username").text(data.user.name);
-    const tweeterHandle = $("<a>").attr("href", "#").addClass("tweeter-handle").text(data.user.handle);
+    const tweeterHandle = createLink().addClass("tweeter-handle").text(data.user.handle);
 
-    // Contructs img inside link tag
-    const img = $("<img>").addClass("avatar").attr("src", data.user.avatars.small);
-
-    avatarLink.append(img);
-    header.append(avatarLink).append(tweeterUsername).append(tweeterHandle);
-
+    header.append(avatar).append(tweeterUsername).append(tweeterHandle);
 
     // Contructs tweet content
     const tweetContent = $("<p>").addClass("tweet-content").text(data.content.text);
@@ -36,21 +31,15 @@ $(function() {
     const tweetButtons = $("<div>").addClass("tweet-buttons");
 
     // Retweet button
-    const retweetLink = createLink();
-    const retweetButton = $("<span>").addClass("fa fa-retweet");
-    retweetLink.append(retweetButton);
+    const retweetButton = createLink().append($("<span>").addClass("fa fa-retweet"));
 
     // Report button
-    const reportLink = createLink();
-    const reportLinkButton = $("<span>").addClass("fa fa-flag-checkered");
-    reportLink.append(reportLinkButton);
+    const reportButton = createLink().append($("<span>").addClass("fa fa-flag-checkered"));
 
     // Like button
-    const likeLink = createLink();
-    const likeLinkButton = $("<span>").addClass("fa fa-heart")
-    likeLink.append(likeLinkButton);
+    const likeButton = createLink().append($("<span>").addClass("fa fa-heart"));
 
-    tweetButtons.append(retweetLink).append(reportLink).append(likeLink);
+    tweetButtons.append(retweetButton).append(reportButton).append(likeButton);
     footer.append(timestamp).append(tweetButtons);
 
     // Puts it all together!
@@ -59,7 +48,6 @@ $(function() {
    return $tweet;
   }
 
-
   function renderTweets(tweets) {
     for (let i = 0; i < tweets.length; i++) {
       createTweetElement(tweets[i]);
@@ -67,53 +55,29 @@ $(function() {
     }
   }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  function loadTweets() {
+     $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: function(data) {
+        renderTweets(data);
       },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
+    });
   }
-]
+
+  loadTweets();
+
+  // POSTs new tweet
+  $("form").on("submit", function( event ) {
+    event.preventDefault();
+    $.post("/tweets", $(this).serialize(), function(data) {
+
+    });
+  });
 
 
-renderTweets(data);
+
+
 });
+
