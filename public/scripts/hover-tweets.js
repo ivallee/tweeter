d59fc56be180cc56b153a3ed44be24a13842743c
Isaac Vallee <isaacvallee@gmail.com>
2017-09-20 00:30:55 +0000

    change hover state to use CSS and clean up CSS

--- public/scripts/hover-tweets.js
+++ public/scripts/hover-tweets.js
@@ -3,7 +3,7 @@ $(function() {
   const tweet = $(".tweet");
   const buttons = $(".tweetButtons");
 
-  // Sets hidden/visible state for buttons
+// Sets hidden/visible state for buttons
   buttons.hide();
   tweet.css("opacity", "0.7")
   $(".tweet").on("mouseenter", function () {
