02bfea2f0fc52aa1b8853c223ef35ec3590f864e
Isaac Vallee <isaacvallee@gmail.com>
2017-09-21 22:40:57 +0000

    linted

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-// const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
@@ -17,10 +14,10 @@ module.exports = function makeDataHelpers(db) {
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       db.collection("tweets").find().toArray((err, tweets) => {
-      const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-      callback(null, tweets.sort(sortNewestFirst));
-    });
-  }
+        const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+        callback(null, tweets.sort(sortNewestFirst));
+      });
+    }
 
-}
-}
+  };
+};
