61137b0fee3c81a6981f62205ffcc11f4f77599f
Isaac Vallee <isaacvallee@gmail.com>
2017-09-21 23:22:39 +0000

    cleaned up and linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
   $(".tweet-area").on("input", function(event) {
     const counter = $(event.target).parent().find(".counter");
-    const charLength = $(event.target).val().length;
+    const tweetLength = $(event.target).val().length;
     const maxCount = 140;
-    counter.text(maxCount - charLength);
-    if (charLength > maxCount) {
+    counter.text(maxCount - tweetLength);
+    if (tweetLength > maxCount) {
       counter.addClass("counter-over");
     } else {
       counter.removeClass("counter-over");
