2c55af4fc791a31cd76ab4b2eadc3d7ee38f43c7
Isaac Vallee <isaacvallee@gmail.com>
2017-09-21 21:52:02 +0000

    add mongo DB module

---
+++ server/lib/db-mongo.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+module.exports = function connect(cb) {
+  MongoClient.connect(MONGODB_URI, cb);
+};
